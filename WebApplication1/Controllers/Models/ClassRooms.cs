4c1bb606b69c7f18b934afef8285792c70a1d464
TARAJESHA <rajeshata2019@gmail.com>
2023-02-18 20:53:50 +0530

    commit 1-woking

---
+++ WebApplication1/Controllers/Models/ClassRooms.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Controllers.Models
+{
+    public class ClassRooms
+    {
+        [Key]
+        public int Id { get; set; }
+        public string? clasName { get; set; }
+        public string? ID { get; set;}
+    }
+}
---
+++ WebApplication1/Controllers/UserController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Context;
+using WebApplication1.Controllers.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly AppDbContext _authContext;
+        public UserController(AppDbContext appDbContext)
+        {
+            _authContext = appDbContext;
+        }
+
+
+
+        [HttpPost("authenticate")]
+        public async Task<IActionResult> Authenticate([FromBody] User userObj)
+        {
+            if(userObj  == null)
+            {
+                return BadRequest();
+            }
+            var user = await _authContext.Users.FirstOrDefaultAsync(x => x.Email == userObj.Email && x.Password == userObj.Password);
+            if (user == null) {
+                return NotFound(new { Message = "User Not Found!" });
+            }
+            return Ok(new
+            {
+                Message="login Success"
+            });
+        }
+        [HttpPost("register")]
+        public async Task<IActionResult> RegisterUser([FromBody] User userOb)
+        {
+            if(userOb == null)
+            {
+                return BadRequest();
+            }
+            await _authContext.Users.AddAsync(userOb);
+            await _authContext.SaveChangesAsync();
+            return Ok(new
+            {
+                Message = "user registerd!"
+            });
+        }
+
+        /*.........................................................*/
+        
+
+    }
+}
